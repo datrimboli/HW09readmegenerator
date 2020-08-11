bf84ab127de4671ca14d21a34cf1bf751a4c54d3
Dave Trimboli <davidatrimboli@gmail.com>
2020-08-11 19:05:50 -0400

    require fs

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const inquirer = require('inquirer')
+
+const fs = require('fs')
+
+const question = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'Pick a number',
+        choices: ['one', 'two', 'three', 'four']
+      }
+    ])
+    .then(res => {
+      console.log(res.name, res.choice)
+
+    })
+    .catch(err => console.log(err))
+}
+
+question()
