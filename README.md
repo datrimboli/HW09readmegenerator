6e0c3ae8fbe81cee2b5743601326b0ce4b143b3f
Dave Trimboli <davidatrimboli@gmail.com>
2020-08-12 21:40:48 -0400

    comments added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# HW09readmegenerator
+# HW09readmegenerator 
+
+In this assignment, we created a README generator
+
+Upon running the app, the user will be presented with a series of questions and prompts to enter information about their project.  The questions and prompts are as follows:
+
+-What is the title of your project?
+-How would you describe your project?
+-Please enter the installation instructions for your project.
+-Please provide the usage information of your project.
+-
+-
+-
+-
+-
+
--- app.js
+++ app.js
@@ -1,56 +1,62 @@
+// Bringing in inquirer
 const inquirer = require('inquirer')
-
+// Bringing in fs
 const fs = require('fs')
 
-
+// Establishing question function
 const question = () => {
+  // Navagating to inquirer prompt
   inquirer
     .prompt([
-      // {
-      //   type: 'input',
-      //   name: 'name',
-      //   message: 'What is your name?'
-      // },
+      // Populating title question in terminal
       {
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?'
       },
+      // Populating description question in terminal
       {
         type: 'input',
         name: 'description',
         message: 'How would you describe your project?'
       },
+      // Populating installation question in terminal
       {
         type: 'input',
         name: 'installation',
         message: 'Please enter the installation instructions for your project.'
       },
+      // Populating usage question in terminal
       {
         type: 'input',
         name: 'usage',
         message: 'Please provide the usage information of your project.'
       },
+      // Populating contribution question in terminal
       {
         type: 'input',
         name: 'contribution',
         message: 'What are the contribution guidelines to your project?'
       },
+      // Populating test question in terminal 
       {
         type: 'input',
         name: 'test',
         message: 'What are the test instructions of your project?'
       },
+      // Populating Github question in terminal
       {
         type: 'input',
         name: 'github',
         message: 'What is your Github username?'
       },
+      // Populating Email question in terminal
       {
         type: 'input',
         name: 'email',
         message: 'What is your email address?'
       },
+      // Populating license question in terminal
       {
         type: 'list',
         name: 'choice',
@@ -58,7 +64,9 @@ const question = () => {
         choices: ['MIT', 'APACHE2.0', 'GPL3.0', 'BSD3', 'None']
       }
     ])
+    // Opening .then function
     .then(res => {
+      // Console logging responses to prompted questions in terminal
       console.log(res.title)
       console.log(res.description)
       console.log(res.installation)
@@ -69,8 +77,10 @@ const question = () => {
       console.log(res.email)
       console.log(res.choice)
 
-
+      // Creating variable "output" which will contain all text populated into my README
+      // Sections & Template literals to populate entered information into the corresponding and correct sections
       var output = `
+     
 # Title: ${res.title}     
  ![badge](https://img.shields.io/badge/license-${res.choice}-blue.svg)                             
 -------
@@ -125,15 +135,26 @@ ${res.choice}
 
 ## Questions:
 
+Project Headquarters:
+12345 Oak Tree St.
+San Diego, CA 91910
+
+Phone:(619) 555-5555
+Fax:(619) 555-5556
+
 Github Username: [${res.github}](https://github.com/${res.github}) 
 
 Contact me via email at: ${res.email} `
+
+      // Write file, creates the "newreadme.md, puts the text in var output into the read me file"
       fs.writeFile('newreadme.md', output, (err) => {
+        // If there is an error, console log err
         if (err) { console.log(err) }
+        // Console logging "File Created!" when a new README file is generated
         console.log('File Created!')
       })
     })
     .catch(err => console.log(err))
 }
-
+// Calling question function
 question()
